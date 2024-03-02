471ab36d60f8243c44d5878a606caf7bfad99a95
Sagar <sagar.mishra.110890@gmail.com>
2024-03-02 13:35:03 +0000

    Lambda Demo

--- src/main/java/org/demo/practice/LambdaDemo.java
+++ src/main/java/org/demo/practice/LambdaDemo.java
@@ -1,2 +1,21 @@
-package org.demo.practice;public class LambdaDemo {
+package org.demo.practice;
+
+import java.util.stream.Stream;
+
+public class LambdaDemo {
+
+    public static void main(String ...args) {
+        LambdaDemo ld = new LambdaDemo();
+        Integer[] myIntegers = {20, 10, -40, 80, 30};
+        Integer sum = ld.getInteger(myIntegers);
+
+        System.out.println(sum);
+    }
+
+    public Integer getInteger(Integer[] myIntegers) {
+        return Stream.of(myIntegers)
+                .reduce(0, Integer::sum);
+//                .reduce(0, (a, b) -> a + b); // same as above
+    }
+
 }
--- src/test/java/org/demo/practice/LambdaDemoTest.java
+++ src/test/java/org/demo/practice/LambdaDemoTest.java
@@ -1,4 +1,19 @@
+package org.demo.practice;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class LambdaDemoTest {
-  
+
+// Test calls for LambdaDemo
+public class LambdaDemoTest {
+
+    LambdaDemo ld;
+
+    @Test
+    void getInteger() {
+        ld = new LambdaDemo();
+        Integer[] myIntegers = {20, 10, -40, 80, 30};
+        Integer sum = ld.getInteger(myIntegers);
+        assertEquals(sum, 100);
+    }
 }
